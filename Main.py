17cb71baa9488226f0f959ddd78806450917666f
reguighamid52-netizen <reguighamid52@gmail.com>
2025-12-20 00:15:46 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class TestApp(App):
+    def build(self):
+        return Label(text='Hello from My Python App!')
+
+if __name__ == '__main__':
+    TestApp().run()
+
